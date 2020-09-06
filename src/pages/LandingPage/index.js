227bf66bc0ed8a4c0da1b0801056c526df131a4d
Yolanda <ymoretto@gmail.com>
2020-09-06 16:13:26 -0300

    lp

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react';
+import {FiLogIn} from 'react-icons/fi';
+import {Container, Content, Background} from './styles.css';
+import Logo from '../../assets/logo.png'; 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { Link } from 'react-router-dom';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Carousel, CardDeck, Card, Button, Row, Col, Image } from 'react-bootstrap';
+import laptop from '../../assets/medical-laptop.jpg'
+import cellphone from '../../assets/cellphone.jpg'
+import hands from '../../assets/hands.jpg'
+import computer from '../../assets/computer.jpg'
+import instagram from '../../assets/instagram.png'
+import email from '../../assets/email.png'
+import github from '../../assets/github.png'
+ 
+export default function LandingPage() {
+        return (
+            <>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home"><img src={Logo} alt="logo"/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav nav-container">
+                    <Nav className="mr-auto nav-space">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="/login">Iniciar Sessão</Nav.Link>
+                      <Nav.Link href="/cadastrar">Criar Conta</Nav.Link>
+                      <Nav.Link href="#">Trabalhe Conosco</Nav.Link>      
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Carousel>
+                <Carousel.Item className="carousel-size">
+                  <img
+                    className="d-block w-100 img-carousel"
+                    src={laptop}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3>Consulta com especialistas</h3>
+                    <p>Compartilhe suas informações com especialistas para continuar seus
+                      cuidados de saúde de forma integral  </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-size">
+                    <img
+                      className="d-block w-100 img-carousel"
+                      src={hands}
+                      alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                      <h3>Compartilhe seus dados de maneira segura</h3>
+                      <p>Autorize o acesso às informações armazenadas com quem
+                        você confia e de maneira segura
+                      </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carousel-size">
+                  <img
+                    className="d-block w-100 img-carousel"
+                    src={cellphone}
+                    alt="Third slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Acesso a qualquer momento</h3>
+                    <p>Baixe e compartilhe seu histórico médico quando precisar. 
+                      Seus dados estão seguros conosco
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
+
+                <h1 className="title-h1">
+                    Serviços
+                </h1>
+
+                <CardDeck className="deck-spacing">
+                    <Card className="card-size">
+                      <Card.Img variant="top" src={computer} />
+                      <Card.Body>
+                        <Card.Title>Histórico Médico</Card.Title>
+                        <Button variant="primary" className="btn-servicos">Saiba mais</Button>
+                       </Card.Body>
+                    </Card>
+                    <Card className="card-size">
+                      <Card.Img variant="top" src={computer} />
+                      <Card.Body>
+                        <Card.Title>Consulta Médica</Card.Title>
+                        <Button variant="primary" className="btn-servicos">Saiba mais</Button>
+                       </Card.Body>
+                    </Card>
+                    <Card className="card-size">
+                      <Card.Img variant="top" src={computer} />
+                      <Card.Body>
+                        <Card.Title>Controle dos seus dados</Card.Title>
+                        <Button variant="primary" className="btn-servicos">Saiba mais</Button>
+                       </Card.Body>
+                    </Card>
+                </CardDeck>
+
+                <Card className="text-center footer">
+                  <Card.Body>
+                    <Card.Title>Políticas de privacidade</Card.Title>
+                    <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <br></br>
+                    Hac sed pellentesque donec mattis auctor urna, mi.
+                    <br></br>Sodales augue aenean tellus mattis cras non.
+                    </Card.Text>
+                    <Card.Title>Redes Sociais</Card.Title>
+              
+                        <Image src={instagram}  className="icon-color"/>
+                      
+                        <Image src={email}  className="icon-color"/>
+
+                       <Image src={github}  className="icon-color"/>
+
+                    <Card.Header className="header">DrivePrev® | 2020</Card.Header>
+                  </Card.Body>
+                </Card>
+
+            </>
+          );
+}
+      
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import LandingPage from './pages/LandingPage';
 
 export default function Routes(){
   return (
     <BrowserRouter>
-        <Route path="/" exact component={SignIn} />
+        <Route path="/login" exact component={SignIn} />
         <Route path="/cadastrar" component={SignUp} />
+        <Route path="/" component={LandingPage} />
     </BrowserRouter>
   );
 }
